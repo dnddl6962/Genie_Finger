2f6c67787adda61249f2df9bfe39ef4d8d06723d
dony1220 <96913965+dony1220@users.noreply.github.com>
2024-01-29 15:33:09 +0900

    Update eng2.py

--- tools/eng2.py
+++ tools/eng2.py
@@ -2,6 +2,8 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import time
 from pynput.keyboard import Controller, Key
+#해당 모듈 또한 가상키보드를 이용하여
+#가상 키보드와 사용자간의 상호작용을 근간으로 한다.
 
 
 class HandTrackingModule:
@@ -51,7 +53,7 @@ class HandTrackingModule:
                     last_click_time = current_time
 
         return img, last_click_time
-
+#해당 클래스는 HandTrackingmodule로 손을 추적하는 프레임을 생성한다.
 
 class Button:
     def __init__(self, pos, text, size=[100, 100]):
@@ -73,7 +75,7 @@ buttonList = [
     Button([1000, 450], "Delete", size=[250, 100]),  # 오른쪽 아래 Delete 버튼, 고정시킨다.
     Button([45, 450], "Enter", size=[250, 100])    # 왼쪽 아래 Enter 버튼
 ]
-
+#해당 버튼 클래스는 영어 알파벳을 생성할 수 있는 클래스이며
 
 def generate_english2_frames():
     cap = cv2.VideoCapture(0)
@@ -96,3 +98,5 @@ def generate_english2_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
 
     cap.release()
+#위의 함수들을 접목하여 웹 캠과 가상키보드를 함께 출력할 수 있게끔
+#Flask에 띄우기 위해서 적절하게 수정을 진행하였다.
